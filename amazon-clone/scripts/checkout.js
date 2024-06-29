8c9fea1bb4d4b2fe37c27dc4900ec4ce2eb06c82
Dnyanesh Pise <gitdnyanesh@hotmail.com>
2024-06-29 15:14:12 +0530

    Fixed checkout option added formatcurrency method

--- amazon-clone/scripts/checkout.js
+++ amazon-clone/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummarryHTML ='';
 cart.forEach((cartItem)=>{
@@ -27,7 +28,7 @@ cart.forEach((cartItem)=>{
           ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $${matchingProduct.priceCents /100}
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -49,7 +50,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -62,7 +63,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -75,7 +76,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
