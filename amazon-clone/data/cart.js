65621eeb148269f0b775b1edfcc7d99759dec8d5
Dnyanesh Pise <gitdnyanesh@hotmail.com>
2024-06-30 11:35:45 +0530

    Added jasime library for automated testing

--- amazon-clone/data/cart.js
+++ amazon-clone/data/cart.js
@@ -1,4 +1,9 @@
-export let cart=JSON.parse(localStorage.getItem("cart")) || [{
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart =JSON.parse(localStorage.getItem("cart")) || [{
     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:3,
     deliveryOptionId :"1"
@@ -7,7 +12,7 @@ export let cart=JSON.parse(localStorage.getItem("cart")) || [{
     quantity:5,
     deliveryOptionId :"2"
 }];
-
+}
 
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart))
